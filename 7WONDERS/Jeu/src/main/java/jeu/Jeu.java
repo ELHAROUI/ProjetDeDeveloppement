6134a78a28c557fa7925992e2a0a78f744510531
Nicolas0694 <ndrisset@outlook.fr>
2019-03-20 20:26:15 +0100

    maj jeu.java

--- 7WONDERS/Jeu/src/main/java/jeu/Jeu.java
+++ 7WONDERS/Jeu/src/main/java/jeu/Jeu.java
@@ -1,12 +1,23 @@
-package jeu;
+class jeu {
+
+    public static void main (String []args) {
 
 
-public static void main(String [] args) {
-	
-	
-	
-	
-	
-	
-	
+        typeRessource [] allTypes = typeRessource.values();
+        typeRessource randomType = randomElement(allTypes);
+
+        Ressource res1 = new Ressource(0,0,randomType);
+
+        Plateau p1= new Plateau (3,0,res1);
+        
+        p1.jouerCarte();
+
+        p1.toString();
+
+
+
+
+
+    }
+
 }
