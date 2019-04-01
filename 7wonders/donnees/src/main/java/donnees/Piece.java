333d913722dae27abca52ec3c0a5234a9898759a
ZakariaG92 <zakaricia@gmail.com>
2019-04-01 13:41:53 +0200

    "ajout de la calsse piéce pour nous permettre de generer de l'argent aux joueurs "

---
+++ 7wonders/donnees/src/main/java/donnees/Piece.java
@@ -0,0 +1,24 @@
+package donnees;
+
+public class Piece {
+
+
+    private int Val;
+
+	
+    public Piece(int VAL) {
+    	this.setVal(VAL);
+    	
+	}
+
+
+	public int getVal() {
+		return Val;
+	}
+
+
+	public void setVal(int val) {
+		Val = val;
+	}
+ 
+}
