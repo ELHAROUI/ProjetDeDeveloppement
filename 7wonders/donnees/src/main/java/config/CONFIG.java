a1b509ebe3385873711f2f5cacaab364228dfeb6
Nicolas0694 <ndrisset@outlook.fr>
2019-04-08 15:35:29 +0200

    commit pour rectification de l'erreur due  au numéro de port

--- 7wonders/donnees/src/main/java/config/CONFIG.java
+++ 7wonders/donnees/src/main/java/config/CONFIG.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package config;
 
 public class CONFIG {
@@ -8,14 +7,3 @@ public class CONFIG {
 
     public static final int NB_JOUEURS = 4;
 }
-=======
-package config;
-
-public class CONFIG {
-
-    public static final String IP = "127.0.0.1";
-    public static final int PORT = 8081;
-
-    public static final int NB_JOUEURS = 4;
-}
->>>>>>> branch 'master' of https://github.com/ELHAROUI/ProjetDeDeveloppement.git
