be6f9e2e7e82ed9aa8d4c31524f41230bb810d14
ELHAROUI <jassim.elharoui@gmail.com>
2019-04-11 22:26:34 +0200

    "régler qlq erreurs"

--- 7wonders/donnees/src/main/java/config/CONFIG.java
+++ 7wonders/donnees/src/main/java/config/CONFIG.java
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/ELHAROUI/ProjetDeDeveloppement.git
 package config;
 
 public class CONFIG {
 
     public static final String IP = "127.0.0.1";
     public static final int PORT = 3020;
-
-<<<<<<< HEAD
-    public static final int NB_JOUEURS = 4;
-}
-=======
     public static final int NB_JOUEURS = 3;
     
 }
->>>>>>> branch 'master' of https://github.com/ELHAROUI/ProjetDeDeveloppement.git
