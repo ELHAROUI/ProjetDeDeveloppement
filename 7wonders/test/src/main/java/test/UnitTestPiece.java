dd6dfe9d4e020002e8f1da1515898809a84ce2d8
ZakariaG92 <zakaricia@gmail.com>
2019-04-24 23:02:47 +0200

    "essai commit test "

--- 7wonders/test/src/main/java/test/UnitTestPiece.java
+++ 7wonders/test/src/main/java/test/UnitTestPiece.java
@@ -1,5 +1,8 @@
 package test;
 
+import org.junit.Test;
+import junit.framework.Assert;
+
 /**
  * Classe pour test de la classe piece
  * @author root
@@ -7,5 +10,14 @@ package test;
  */
 public class UnitTestPiece {
 	
-
+	@SuppressWarnings("deprecation")
+	@Test 
+	public void testpcs() {
+	//	Piece p = new Piece(5);
+//	int result=	p.getVal();
+	//Assert.assertEquals(5, result);
+		
+	}
+	
+	
 }
