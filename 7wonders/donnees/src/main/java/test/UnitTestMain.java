be874f9a5afb87cbc6e217fd2fe735ff0213b911
ELHAROUI <jassim.elharoui@gmail.com>
2019-04-26 23:01:21 +0200

    Ajout du Test de la classe Main #6

---
+++ 7wonders/donnees/src/main/java/test/UnitTestMain.java
@@ -0,0 +1,65 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import donnees.*;
+import junit.framework.Assert;
+
+
+public class UnitTestMain {
+
+	Main main;
+	private ArrayList<Carte> cartes ;
+	Carte carte1, carte2, carte3;
+	private ArrayList<Piece> pieces ;
+	Piece piece1, piece2, piece3;
+	
+    @Test
+    public void testAjoutCartePiece() {
+    	
+		this.main = new Main();
+
+		this.cartes = new ArrayList<Carte>();
+
+		this.main.setCartes(cartes);
+		
+		this.pieces = new ArrayList<Piece>();
+		
+		this.main.setPieces(pieces);
+		
+        this.main.ajouterCarte(carte1);
+        this.main.ajouterCarte(carte2);
+        this.main.ajouterCarte(carte3);
+        
+        this.main.ajouterPiece(piece1);
+        this.main.ajouterPiece(piece2);
+        this.main.ajouterPiece(piece3);
+
+        /**
+         * test de la taille des cartes dans la main
+         */
+        assertEquals(3, this.main.getCartes().size());
+
+        /**
+         * test le remplissage de la main par les cartes
+         */
+        assertNotNull(this.main.getCartes());
+        
+        /**
+         * test de la taille du main de pieces
+         */
+        assertEquals(3, this.main.getPieces().size());
+        
+        /**
+         * test le remplissage de la main par les pieces
+         */
+        assertNotNull(this.main.getPieces());
+
+    }
+
+
+
+}
